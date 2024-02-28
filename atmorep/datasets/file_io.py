928a9b9b4b04345ab6461cb9d6d6d47c4045c3b3
Kacper Nowak <kacper.nowak@awi.de>
2024-02-28 18:13:28 +0100

    Add support for FESOM files

--- atmorep/datasets/file_io.py
+++ atmorep/datasets/file_io.py
@@ -18,68 +18,79 @@ import torch
 import numpy as np
 import xarray as xr
 
-####################################################################################################
+from atmorep.utils.utils import days_until_month_in_year, days_in_month
 
-def netcdf_file_loader(fname, field, time_padding = [0,0,1], days_in_month = 0, static=False) :
-  
-  ds = xr.open_dataset(fname, engine='netcdf4')[field]
-
-  if not static:
-    # TODO: test that only time_padding[0] *or* time_padding[1] != 0
-    if time_padding[0] != 0 :
-      ds = ds[-time_padding[0]*time_padding[2] : ]
-    elif time_padding[1] != 0 :
-      ds = ds[ : time_padding[1]*time_padding[2]]
-    if time_padding[2] > 1 :
-      ds = ds[::time_padding[2]]
-
-  x = torch.from_numpy(np.array( ds, dtype=np.float32))
-  ds.close()
-  
-  return x
 
 ####################################################################################################
 
-def grib_file_loader(fname, field, time_padding = [0,0,1], days_in_month = 0, static=False) :
-  
-  ds = xr.open_dataset(fname, engine='cfgrib',
-                              backend_kwargs={'time_dims':('valid_time','indexing_time')})[field]
+def netcdf_file_loader(fname, field, time_padding=[0, 0, 1], fesom=0, static=False):
+    ds = xr.open_dataset(fname, engine='netcdf4')[field]
+    # In case of FESOM data fesom argument is used to pass (year, month, level) tuple
+    # This is done to ensure backwards compatibilty
+    if not isinstance(days_in_month, int):
+        (year, month, level) = fesom
+        print(fesom)
+        start = days_until_month_in_year(year, month)
+        print(days_in_month(year, month))
+        end = start + days_in_month(year, month)
+        ds = ds[start:end, level, :, :]
+    else:
+        if not static:
+            # TODO: test that only time_padding[0] *or* time_padding[1] != 0
+            if time_padding[0] != 0:
+                ds = ds[-time_padding[0] * time_padding[2]:]
+            elif time_padding[1] != 0:
+                ds = ds[: time_padding[1] * time_padding[2]]
+            if time_padding[2] > 1:
+                ds = ds[::time_padding[2]]
 
-  # work-around for bug in download where for every month 31 days have been downloaded
-  if days_in_month > 0 :
-    ds = ds[:24*days_in_month]
+    x = torch.from_numpy(np.array(ds, dtype=np.float32))
+    ds.close()
 
-  if not static:
-    # TODO: test that only time_padding[0] *or* time_padding[1] != 0
-    if time_padding[0] != 0 :
-      ds = ds[-time_padding[0]*time_padding[2] : ]
-    elif time_padding[1] != 0 :
-      ds = ds[ : time_padding[1]*time_padding[2]]
-    if time_padding[2] > 1 :
-      ds = ds[::time_padding[2]]
+    return x
 
-  x = torch.from_numpy(np.array(ds, dtype=np.float32))
-  ds.close()
-  
-  # assume grib files are clean and NaNs are introduced through handling of "missing values"
-  if np.isnan( x).any() :
-    x_shape = x.shape
-    x = x.flatten()
-    x[np.argwhere( np.isnan( x))] = 9999.
-    x = np.reshape( x, x_shape)
-  
-  return x
 
 ####################################################################################################
 
-def bin_file_loader( fname, field, time_padding = [0,0,1], static=False, file_shape = (-1, 721, 1440)) :
+def grib_file_loader(fname, field, time_padding=[0, 0, 1], days_in_month=0, static=False):
+    ds = xr.open_dataset(fname, engine='cfgrib',
+                         backend_kwargs={'time_dims': ('valid_time', 'indexing_time')})[field]
 
-  ds = np.fromfile(fname,  dtype=np.float32) 
-  print("INFO:: reshaping binary file into {}".format(file_shape))   
-  if not static: 
-    ds = np.reshape(ds, file_shape)
-    ds = ds[time_padding[0]:(ds.shape[0] - time_padding[0])]
-  else:
-    ds = np.reshape(ds, file_shape[1:])
-  x = torch.from_numpy(ds)
-  return x
+    # work-around for bug in download where for every month 31 days have been downloaded
+    if days_in_month > 0:
+        ds = ds[:24 * days_in_month]
+
+    if not static:
+        # TODO: test that only time_padding[0] *or* time_padding[1] != 0
+        if time_padding[0] != 0:
+            ds = ds[-time_padding[0] * time_padding[2]:]
+        elif time_padding[1] != 0:
+            ds = ds[: time_padding[1] * time_padding[2]]
+        if time_padding[2] > 1:
+            ds = ds[::time_padding[2]]
+
+    x = torch.from_numpy(np.array(ds, dtype=np.float32))
+    ds.close()
+
+    # assume grib files are clean and NaNs are introduced through handling of "missing values"
+    if np.isnan(x).any():
+        x_shape = x.shape
+        x = x.flatten()
+        x[np.argwhere(np.isnan(x))] = 9999.
+        x = np.reshape(x, x_shape)
+
+    return x
+
+
+####################################################################################################
+
+def bin_file_loader(fname, field, time_padding=[0, 0, 1], static=False, file_shape=(-1, 721, 1440)):
+    ds = np.fromfile(fname, dtype=np.float32)
+    print("INFO:: reshaping binary file into {}".format(file_shape))
+    if not static:
+        ds = np.reshape(ds, file_shape)
+        ds = ds[time_padding[0]:(ds.shape[0] - time_padding[0])]
+    else:
+        ds = np.reshape(ds, file_shape[1:])
+    x = torch.from_numpy(ds)
+    return x
